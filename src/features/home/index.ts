ba479da03e11866c55adf6052a5867e1c15f41ef
JorgeAntonio <jorge.antonio.delaguila@gmail.com>
2026-02-20 12:46:07 -0500

    feat: Implement initial EPG portal structure with core pages, navigation, and home components.

--- src/features/home/index.ts
+++ src/features/home/index.ts
@@ -1,8 +1,4 @@
-export { AdmissionCTA } from './components/AdmissionCTA';
 export { ContactSection } from './components/ContactSection';
-export { FeaturedPrograms } from './components/FeaturedPrograms';
-export { Hero } from './components/Hero';
-export { NewsAndEvents } from './components/NewsAndEvents';
 export { ProgramSearch } from './components/ProgramSearch';
 export { ProgramSearchWithStats } from './components/ProgramSearchWithStats';
 export { StatsSection } from './components/StatsSection';
